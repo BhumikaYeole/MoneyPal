02428caa845384793a36177b04698d24fe9c7df2
BhumikaYeole <yeolebhumika@gmail.com>
2025-08-14 17:16:14 +0530

    final changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,37 +19,7 @@ function HeroSection() {
           <p className="text-xl md:text-4xl mb-8 text-gray-600 max-w-2xl mx-auto leading-relaxed">
             Your wallet's very own smart assistant
           </p>
-            <div className="flex justify-center mb-8">
-              <SignInButton forceRedirectUrl='/dashboard'>
-                <Button  variant="outline" className="mr-4" >
-                    Get Started
-                </Button>   
-              </SignInButton>
-                <Button onClick={() => setIsOpen(true)}>
-                    Watch Demo
-                </Button>
-            </div>
-
-             {isOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-70 flex flex-col justify-center items-center z-50"
-          onClick={() => setIsOpen(false)} // click outside closes
-        >
-          <div
-            className="bg-white rounded-lg overflow-hidden shadow-lg max-w-3xl w-full relative"
-            onClick={(e) => e.stopPropagation()} // stop outside click
-          >
-            <button
-              onClick={() => setIsOpen(false)}
-              className="absolute top-1 right-1 text-white text-2xl font-bold hover:text-red-500"
-            >
-              X
-            </button>
-            <video src="/videos/MoneyPalDemo.mp4" controls autoPlay className="w-full" />
-          </div>
-          <p className="text-white">Click anywhere to exit</p>
-        </div>
-      )}
+            
           <div className="relative max-w-6xl mx-auto">
             <div className="absolute -inset-4 bg-gradient-to-r from-blue-600 to-blue-400 rounded-2xl blur-2xl opacity-20"></div>
             <div className="relative bg-white/90 backdrop-blur-sm rounded-2xl shadow-2xl border border-gray-200/50 p-6">
